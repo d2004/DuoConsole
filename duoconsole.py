75736c0a54397a92244a283488240fccf2fdc823
d2004 <davideramondetti@yahoo.it>
2015-07-06 22:45:25 +0200

    Update duoconsole.py

--- duoconsole.py
+++ duoconsole.py
@@ -1,1 +1,29 @@
+"""
+RamondettiDavide Spa
+====================
+DuoConsole Python 20
 
+DuoConsole is a free open source console in python and it execute some commands
+"""
+import sys
+
+print("************************");
+print("* RamondettiDavide Spa *");
+print("* DuoConsole Python 20 *");
+print("************************");
+def CommandLine() :
+  command = input("DuoConsole:~ root$ ");
+  if (command == null) :
+    print("DuoConsole Cannot Execute This Code");
+  else :
+    if (command == "print") :
+      wr = input("DuoConsole:~ root$ print ");
+      print(wr);
+    if (command == "echo") :
+      print("To execute the command ECHO you must buy DuoConsole Platinum!");
+    if (command == "whois") :
+      whois = input("DuoConsole:~ root$ whois ");
+      open("http://who.is/whois/",whois);
+    else :
+      print("Buy the PRO to Use Some Codes.");
+CommandLine();
